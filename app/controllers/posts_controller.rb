77fd4ac82757f271bfdf57c8073adecd21204909
parisa <parisa.golanbary@gmail.com>
2018-09-12 12:31:20 +0430

    Add devise and Users.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@
 
 # this is PostsController
 class PostsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   def index
     @posts = Post.all.order('created_at DESC')
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@
 
 # this is routes
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts do
     resources :comments
